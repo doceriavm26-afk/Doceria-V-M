a1153875d936821851256fd1b6b342692729d3b5
doceriavm26-afk <doceriavm26@gmail.com>
2025-09-27 20:36:12 -0300

    carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('.cardapio');
+const prevBtn = document.querySelector('.btn-prev');
+const nextBtn = document.querySelector('.btn-next');
+
+// Scroll automático
+let autoScroll = setInterval(() => {
+    if(container.scrollLeft + container.clientWidth >= container.scrollWidth){
+        container.scrollTo({ left:0, behavior:'smooth' });
+    } else {
+        container.scrollBy({ left: container.clientWidth/3, behavior:'smooth' });
+    }
+},3000);
+
+// Botões
+nextBtn.addEventListener('click', ()=>{
+    container.scrollBy({ left: container.clientWidth/3, behavior:'smooth' });
+    resetAutoScroll();
+});
+prevBtn.addEventListener('click', ()=>{
+    container.scrollBy({ left: -container.clientWidth/3, behavior:'smooth' });
+    resetAutoScroll();
+});
+
+function resetAutoScroll(){
+    clearInterval(autoScroll);
+    autoScroll = setInterval(() => {
+        if(container.scrollLeft + container.clientWidth >= container.scrollWidth){
+            container.scrollTo({ left:0, behavior:'smooth' });
+        } else {
+            container.scrollBy({ left: container.clientWidth/3, behavior:'smooth' });
+        }
+    },3000);
+}
+
+// Swipe mobile
+let startX;
+container.addEventListener('touchstart', (e)=> startX = e.touches[0].clientX);
+container.addEventListener('touchend', (e)=>{
+    let endX = e.changedTouches[0].clientX;
+    if(startX - endX > 50) container.scrollBy({ left: container.clientWidth/3, behavior:'smooth' });
+    if(endX - startX > 50) container.scrollBy({ left: -container.clientWidth/3, behavior:'smooth' });
+    resetAutoScroll();
+});
